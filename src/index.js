656b5583a897200ca87bcf442d2b8d0682862ea2
Sai Madan <thakursaimadan@gmail.com>
2024-08-02 15:01:29 +0530

    addede body parser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express=require('express');
-
+const bodyParser=require('body-parser')
 const ServerConfig=require('./config/serverconfig');
 const connectDB = require('./config/dbconfig');
 
 const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
 
 app.listen(ServerConfig.PORT,async ()=>{
     
     await connectDB();
     console.log("server starded at ",ServerConfig.PORT);
-    
-}) 
+
+})   
